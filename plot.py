174b5e78807a1e3cb3dfe8142f31ab5da3a04ee2
Matthew Marshall <matthew.marshall090396@gmail.com>
2021-05-04 23:28:10 +0100

    Script to create gifs from results of ant colony optimisation.

---
+++ plot.py
@@ -0,0 +1,44 @@
+from csv import reader
+# import matplotlib
+# matplotlib.use("WebAgg")
+import matplotlib.animation as anim
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+def plot_graph(data_source: str, output: str):
+    results = []
+    with open(data_source, "r") as f:
+        csv = reader(f)
+        for row in csv:
+            row_as_floats = []
+            for entry in row:
+                row_as_floats.append(float(entry))
+            results.append(row_as_floats)
+
+    results_as_matrix = []
+    for result in results:
+        results_as_matrix.append(np.reshape(result, (8, 8)))
+
+    # im = plt.imshow(results_as_matrix[0], cmap="hot", interpolation="nearest")
+    # plt.show()
+
+    fig = plt.figure()
+    ax = sns.heatmap(results_as_matrix[0], vmin=0, vmax=25)
+
+    def init():
+        plt.clf()
+        ax = sns.heatmap(results_as_matrix[0], vmin=0, vmax=25)
+
+    def animate(i):
+        plt.clf()
+        ax = sns.heatmap(results_as_matrix[i], vmin=0, vmax=25)
+
+    animation = anim.FuncAnimation(fig, animate, init_func=init, frames=1000, repeat=False, interval=0)
+
+    # plt.show()
+
+    animation.save(output, writer="imagemagick", fps=60)
+
+plot_graph("results/pheromone_result.csv", "results/pheromone_result.gif")
+plot_graph("results/ants_result.csv", "results/ants_result.gif")
