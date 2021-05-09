5f7f4e579ba2013eeed100d68a8b5c93e012a114
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2021-05-09 12:31:12 +0100

    Make plot.py more useful for upcoming artifacts and testing multiple algorithms.

--- plot.py
+++ plot.py
@@ -1,10 +1,14 @@
+import argparse
 from csv import reader
+
 import matplotlib
 matplotlib.use("Agg")
+
 import matplotlib.animation as anim
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+
 
 def plot_graph(data_source: str, output: str):
     results = []
@@ -18,27 +22,34 @@ def plot_graph(data_source: str, output: str):
 
     results_as_matrix = []
     for result in results:
-        results_as_matrix.append(np.reshape(result, (8, 8)))
-
-    # im = plt.imshow(results_as_matrix[0], cmap="hot", interpolation="nearest")
-    # plt.show()
+        results_as_matrix.append(np.reshape(result, (51, 51)))
 
     fig = plt.figure()
-    ax = sns.heatmap(results_as_matrix[0], vmin=0, vmax=25)
+    ax = sns.heatmap(results_as_matrix[0], vmin=0, vmax=200)
 
     def init():
         plt.clf()
-        ax = sns.heatmap(results_as_matrix[0], vmin=0, vmax=25)
+        ax = sns.heatmap(results_as_matrix[0], vmin=0, vmax=200)
 
     def animate(i):
         plt.clf()
-        ax = sns.heatmap(results_as_matrix[i], vmin=0, vmax=25)
+        ax = sns.heatmap(results_as_matrix[i], vmin=0, vmax=200)
 
-    animation = anim.FuncAnimation(fig, animate, init_func=init, frames=1000, repeat=False, interval=0)
-
-    # plt.show()
+    animation = anim.FuncAnimation(fig, animate, init_func=init, frames=2000, repeat=False, interval=0)
 
     animation.save(output, writer="imagemagick", fps=60)
 
-plot_graph("results/pheromone_result.csv", "results/pheromone_result.gif")
-plot_graph("results/ants_result.csv", "results/ants_result.gif")
+
+def plot_graphs(algorithm: str, count: int):
+    for i in range(count):
+        plot_graph(f"results/{i}.{algorithm}.pheromone_result.csv", f"results/{i}.{algorithm}.pheromone_result.gif")
+        plot_graph(f"results/{i}.{algorithm}.ants_result.csv", f"results/{i}.{algorithm}.ants_result.gif")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--algo", type=str, required=True)
+    parser.add_argument("--count", type=int, required=True)
+
+    args = parser.parse_args()
+
+    plot_graphs(args.algo, args.count)
