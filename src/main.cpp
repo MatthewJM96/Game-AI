c3c7ad04917181fa156bb5aa68f9655ed9b6a2c1
Matthew Marshall <matthew.marshall090396@gmail.com>
2021-05-22 04:57:50 +0100

    Add 15x15 map test func.

--- src/main.cpp
+++ src/main.cpp
@@ -113,6 +113,63 @@ void do_iteration_count_test() {
     }
 }
 
+void do_map_15_test(size_t map_idx) {
+    const size_t map_dim       =   31;
+    const size_t max_steps     =  200;
+    const size_t ant_count     =   50;
+    const size_t iterations    = 1000;
+    const aco::acs::ACSOptions::OutputFreq output_frequency = {
+        40, 1
+    };
+
+    const float global_pheromone_increment   = 1.0f; // Global increment (best ant in round or all rounds).
+    const float global_pheromone_evaporation = 0.1f; // Global decrement on each node per round.
+    const float pheromone_increment          = 1.0f / (float)map_dim; // Local increment (per ant per node) per timestep.
+    const float pheromone_evaporation        = 0.1f; // Global decrement on each node per timestep.
+
+    const float exploitation_factor = 0.9f;
+    const float cost_exponent = 2.0f;
+
+    std::string idx_str = std::to_string(map_idx);
+
+    map::maze2d::Map<map_dim + 2> halo_map;
+
+    halo_map = map::maze2d::load_map_with_halo<map_dim>("maps/15." + idx_str + ".solved.map");
+
+    std::cout << "Map " << map_idx + 1 << " of dim 15 maps, with ideal solution length " << halo_map.solution_length << ":\n" << std::endl;
+
+    map::maze2d::print_map<map_dim + 2>(halo_map);
+
+    map::maze2d::GraphMap graph_map = map::maze2d::map_to_graph<>(halo_map, 1.0f);
+
+    const aco::acs::ACSOptions options {
+        idx_str,
+        iterations,
+        {
+            map_dim + 2,
+            map_dim + 2
+        },
+        max_steps,
+        ant_count,
+        exploitation_factor,
+        cost_exponent,
+        {
+            pheromone_increment,
+            pheromone_evaporation
+        },
+        {
+            global_pheromone_increment,
+            global_pheromone_evaporation
+        },
+        true,
+        output_frequency,
+        0,
+        nullptr
+    };
+
+    size_t iterations_to_ideal_solution = aco::acs::do_simulation(graph_map, options);
+}
+
 void do_map_25_test(size_t map_idx) {
     const size_t map_dim       =  51;
     const size_t max_steps     = 300;
@@ -173,7 +230,9 @@ void do_map_25_test(size_t map_idx) {
 int main() {
     std::cout << "Hello, world!" << std::endl;
 
-    do_map_25_test(0);
+    do_map_15_test(1);
+
+    // do_map_25_test(0);
 
     // do_iteration_count_test();
 }
